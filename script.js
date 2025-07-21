4cfcfb18cb5e5735939efe4662de490621898d87
Sakey01 <sheikh36ahmed@gmail.com>
2025-07-21 23:17:37 +0100

    high score stored and timer added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const highScore = document.querySelector("#high-score span");
+const timer = document.getElementById("timer");
 const scoreBoard = document.getElementById("score");
 const holes = document.querySelectorAll(".hole");
 const moles = document.querySelectorAll(".mole");
@@ -7,6 +8,10 @@ let lastHole;
 let score = 0;
 let timeUp = false;
 let isStarted = false;
+const timeLimit = 15000;
+
+const storedHighScore = Number(localStorage.getItem("highScore"));
+if (storedHighScore !== null) highScore.textContent = storedHighScore;
 
 function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
@@ -41,19 +46,44 @@ function whack(e) {
   scoreBoard.innerHTML = score;
 }
 
+function endGame() {
+  timeUp = true;
+  isStarted = false;
+  highScore.textContent = Math.max(score, Number(highScore.textContent));
+  const storedHighScore = localStorage.setItem("highScore", JSON.stringify(highScore.textContent));
+}
+
 function startGame() {
-  scoreBoard.textContent = 0;
+  let timeLimit = 15;
   score = 0;
   timeUp = false;
   isStarted = true;
+
+  timer.textContent = `0:${timeLimit < 10 ? '0' : ''}${timeLimit}`;
+
+  const interval = setInterval(() => {
+    timeLimit--;
+    if (timeLimit <= 0) {
+      timer.textContent = "0:00";
+      clearInterval(interval);
+      return;
+    }
+
+    timer.textContent = `0:${timeLimit < 10 ? '0' : ''}${timeLimit}`;
+  }, 1000);
+
   peep();
-  setTimeout(() => (timeUp = true, isStarted = false, highScore.textContent = Math.max(score, Number(highScore.textContent))), 15000);
-  
+  setTimeout(endGame, 15000);
+}
+
+function endGame() {
+  timeUp = true;
+  isStarted = false;
+  highScore.textContent = Math.max(score, Number(highScore.textContent));
+  localStorage.setItem("highScore", highScore.textContent);
 }
 
 moles.forEach((mole) => mole.addEventListener("click", whack));
 btn.addEventListener("click", () => {
   if (!isStarted) startGame();
 });
-
-
